964925068d59fd8c46e467fc6657c9a7fc946d1b
jonathan <aungzayar1352000@gmail.com>
2024-02-12 17:37:36 +0630

    well done

---
+++ src/components/Card.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import CardItem from "./CardItem";
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <div className="cards">
+      <h1>Check out Be's existence</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="images/2.jpg"
+              text="Explore Be' existence!"
+              label="Lovely Be Be"
+              path="/services"
+            />
+            <CardItem
+              src="images/5.jpg"
+              text="Explore Be' existence!"
+              label="Lovely Be Be"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/6.jpg"
+              text="Explore Be' existence!"
+              label="Lovely Be Be"
+              path="/services"
+            />
+            <CardItem
+              src="images/7.jpeg"
+              text="Explore Be' existence!"
+              label="Lovely Be Be"
+              path="/services"
+            />
+            <CardItem
+              src="images/9.jpg"
+              text="Explore Be' existence!"
+              label="Lovely Be Be"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/10.jpg"
+              text="Explore Be' existence!"
+              label="Lovely Be Be"
+              path="/services"
+            />
+            <CardItem
+              src="images/11.jpg"
+              text="Explore Be' existence!"
+              label="Lovely Be Be"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/16.jpg"
+              text="Explore Be' existence!"
+              label="Lovely Be Be"
+              path="/services"
+            />
+            <CardItem
+              src="images/14.jpg"
+              text="Explore Be' existence!"
+              label="Lovely Be Be"
+              path="/services"
+            />
+            <CardItem
+              src="images/15.jpg"
+              text="Explore Be' existence!"
+              label="Lovely Be Be"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/4.jpg"
+              text="Explore Be' existence!"
+              label="Lovely Be Be"
+              path="/services"
+            />
+            <CardItem
+              src="images/3.jpg"
+              text="Explore Be' existence!"
+              label="Lovely Be Be"
+              path="/services"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
